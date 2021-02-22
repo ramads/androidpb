59a8421e2949caa9f3fd76b6103ea308aadbd3c9
Ramaditia D <rdwiyan.saputra@gmail.com>
2021-02-22 10:58:02 +0800

    add keterangan pada contoh aplikas sederhana

--- HelloWorld/app/src/main/java/unram/psti/helloworld/Contoh.java
+++
@@ -1,4 +0,0 @@
-package unram.psti.helloworld;
-
-public class Contoh {
-}
--- HelloWorld/app/src/main/java/unram/psti/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/unram/psti/helloworld/MainActivity.java
@@ -16,8 +16,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
+
+        // melakukan binding resource tampilan design pada layout/activity_main.xml
+        
+        // untuk komponen Edit Text, gunakan tipe data EditText, method findViewById digunakan untuk menemukan resource pada layout/activity_main.xml berdasarkan id yang telah diberikan
         EditText editText = findViewById(R.id.et_input);
+
+        // untuk komponen Text View, gunakan tipe data TextView 
         TextView tvHasil = findViewById(R.id.tv_hasil);
+
+        // set text yang didapatkan dari komponen edit text ke komponen text view hasil
         tvHasil.setText(editText.getText().toString());
 
     }
